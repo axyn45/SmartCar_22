6d3b6138e3a02693a02bea8f3353946c015851e0
Alex Yuan <axyn45@outlook.com>
2022-05-04 16:18:57 +0800

    Motor adjustment

--- src/AppSw/Tricore/User/LQ_ImageProcess.c
+++ src/AppSw/Tricore/User/LQ_ImageProcess.c
@@ -45,36 +45,36 @@ QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ*/
  *  修改时间：2020年11月18日
  *  备    注：
  *************************************************************************/
-void TFT_Show_Camera_Info (void)
+void TFT_Show_Camera_Info(void)
 {
     char txt[16] = "X:";
 
-    sint16 mps = 0, dmm = 0;    // 速度：m/s,毫米数值
+    sint16 mps = 0, dmm = 0; // 速度：m/s,毫米数值
     sint16 pulse100 = 0;
     uint16 bat = 0;
 
-    dmm = (sint16) (RAllPulse * 100 / 579);         // 龙邱512带方向编码器1米5790个脉冲，数值太大，除以100
-    pulse100 = (sint16) (RAllPulse / 100);
-    sprintf(txt, "AP:%05d00", pulse100);           //
-    TFTSPI_P8X16Str(3, 4, txt, u16RED, u16BLUE);   // 显示赛道偏差参数
+    dmm = (sint16)(RAllPulse * 100 / 579); // 龙邱512带方向编码器1米5790个脉冲，数值太大，除以100
+    pulse100 = (sint16)(RAllPulse / 100);
+    sprintf(txt, "AP:%05d00", pulse100);         //
+    TFTSPI_P8X16Str(3, 4, txt, u16RED, u16BLUE); // 显示赛道偏差参数
 
-    NowTime = (STM_GetNowUs(STM0) - NowTime) / 1000;  // 获取STM0 当前时间，得到毫秒
-    mps = (sint16) (dmm / (NowTime / 1000));          // 计算速度mm/s
+    NowTime = (STM_GetNowUs(STM0) - NowTime) / 1000; // 获取STM0 当前时间，得到毫秒
+    mps = (sint16)(dmm / (NowTime / 1000));          // 计算速度mm/s
     // TFTSPI_Road(18, 0, LCDH, LCDW, (unsigned char *)Image_Use); // TFT1.8动态显示摄像头灰度图像
-    TFTSPI_BinRoad(18, 0, LCDH, LCDW, (unsigned char *) Bin_Image);  // TFT1.8动态显示摄像头二进制图像
+    TFTSPI_BinRoad(18, 0, LCDH, LCDW, (unsigned char *)Bin_Image); // TFT1.8动态显示摄像头二进制图像
     sprintf(txt, "%04d,%04d,%04d", OFFSET0, OFFSET1, OFFSET2);
-    TFTSPI_P8X16Str(0, 5, txt, u16RED, u16BLUE);       // 显示赛道偏差参数
-    BatVolt = ADC_Read(ADC7);  // 刷新电池电压
-    bat = BatVolt * 11 / 25;  // x/4095*3.3*100*5.7
-    sprintf(txt, "B:%d.%02dV %d.%02dm/s", bat / 100, bat % 100, mps / 1000, (mps / 10) % 100);  // *3.3/4095*3
-    TFTSPI_P8X16Str(0, 6, txt, u16WHITE, u16BLUE);   // 字符串显示
+    TFTSPI_P8X16Str(0, 5, txt, u16RED, u16BLUE);                                               // 显示赛道偏差参数
+    BatVolt = ADC_Read(ADC7);                                                                  // 刷新电池电压
+    bat = BatVolt * 11 / 25;                                                                   // x/4095*3.3*100*5.7
+    sprintf(txt, "B:%d.%02dV %d.%02dm/s", bat / 100, bat % 100, mps / 1000, (mps / 10) % 100); // *3.3/4095*3
+    TFTSPI_P8X16Str(0, 6, txt, u16WHITE, u16BLUE);                                             // 字符串显示
     // 电机和舵机参数显示
     sprintf(txt, "Sv:%04d Rno:%d", ServoDuty, CircleNumber);
-    TFTSPI_P8X16Str(1, 7, txt, u16RED, u16BLUE);     // 显示舵机，电机1，编码器1数值
+    TFTSPI_P8X16Str(1, 7, txt, u16RED, u16BLUE); // 显示舵机，电机1，编码器1数值
     sprintf(txt, "M1:%04d, M2:%04d ", MotorDuty1, MotorDuty2);
-    TFTSPI_P8X16Str(0, 8, txt, u16RED, u16BLUE);     // 电机1-2数值
+    TFTSPI_P8X16Str(0, 8, txt, u16RED, u16BLUE); // 电机1-2数值
     sprintf(txt, "E1:%04d, E2:%04d ", ECPULSE1, ECPULSE2);
-    TFTSPI_P8X16Str(0, 9, txt, u16RED, u16BLUE);     // 编码器1-2数值
+    TFTSPI_P8X16Str(0, 9, txt, u16RED, u16BLUE); // 编码器1-2数值
 }
 /*************************************************************************
  *  函数名称：void CameraCar(void)
@@ -87,7 +87,7 @@ void TFT_Show_Camera_Info (void)
  *  修改时间：2020年10月28日
  *  备    注：驱动2个电机
  *************************************************************************/
-void CameraCar (void)
+void CameraCar(void)
 {
     // 摄像头初始化
     CAMERA_Init(50);
@@ -96,54 +96,58 @@ void CameraCar (void)
     delayms(500);
 
     // 切记CPU0,CPU1...不可以同时开启屏幕显示，否则冲突不显示
-    mutexCpu0TFTIsOk = 0;           // CPU1： 0占用/1释放 TFT
+    mutexCpu0TFTIsOk = 0; // CPU1： 0占用/1释放 TFT
     // CircleNumber = SetCircleNum();  // 设置需要进入圆环的个数；
 
     // 。根据需要设置出入库，出库是固定执行，
     // 。入库需要干簧管和外部中断配合实现
     // 。本例程中，干簧管在通过圆环后开启，不会出现起跑触发的可能性
-    //OutInGarage(OUT_GARAGE, ReadOutInGarageMode()); // 测试出库，拨码在上左侧出入库，反之右侧出入库
-    //OutInGarage(IN_GARAGE,ReadOutInGarageMode());  // 测试入库
+    // OutInGarage(OUT_GARAGE, ReadOutInGarageMode()); // 测试出库，拨码在上左侧出入库，反之右侧出入库
+    // OutInGarage(IN_GARAGE,ReadOutInGarageMode());  // 测试入库
 
-    TFTSPI_CLS(u16BLUE);            // 清屏
+    TFTSPI_CLS(u16BLUE); // 清屏
     // 切记CPU0,CPU1...不可以同时开启屏幕显示，否则冲突不显示
-    mutexCpu0TFTIsOk = 1;           // CPU1： 0占用/1释放 TFT
+    mutexCpu0TFTIsOk = 1; // CPU1： 0占用/1释放 TFT
 
-    RAllPulse = 0;                  // 全局变量，脉冲计数总数
-    NowTime = STM_GetNowUs(STM0);   // 获取STM0 当前时间
+    RAllPulse = 0;                // 全局变量，脉冲计数总数
+    NowTime = STM_GetNowUs(STM0); // 获取STM0 当前时间
     MotorInit();
     while (1)
     {
-        LED_Ctrl(LED1, RVS);     // LED闪烁 指示程序运行状态
+        LED_Ctrl(LED1, RVS); // LED闪烁 指示程序运行状态
         if (Camera_Flag == 2)
         {
-            Camera_Flag = 0;     // 清除摄像头采集完成标志位  如果不清除，则不会再次采集数据
-            Get_Use_Image();     // 取出赛道及显示所需图像数据
-            Get_Bin_Image(0);    // 转换为01格式数据，0、1原图；2、3边沿提取
-            Bin_Image_Filter();  // 滤波，三面被围的数据将被修改为同一数值
+            Camera_Flag = 0;    // 清除摄像头采集完成标志位  如果不清除，则不会再次采集数据
+            Get_Use_Image();    // 取出赛道及显示所需图像数据
+            Get_Bin_Image(0);   // 转换为01格式数据，0、1原图；2、3边沿提取
+            Bin_Image_Filter(); // 滤波，三面被围的数据将被修改为同一数值
 
-            TFTSPI_BinRoad(0, 0, LCDH, LCDW, (unsigned char *) Bin_Image);
-            Seek_Road();         // 通过黑白区域面积差计算赛道偏差值
+            TFTSPI_BinRoad(0, 0, LCDH, LCDW, (unsigned char *)Bin_Image);
+            Seek_Road(); // 通过黑白区域面积差计算赛道偏差值
             FindEdge();
 
             // 计算赛道偏差值，系数越大打角越早，数值跟舵机的范围有关，此处为±160左右，默认为7，
-            ServoDuty = Servo_Center_Mid - (OFFSET1 + OFFSET2 + OFFSET2) * 1 / 3;
+            ServoDuty = Servo_Center_Mid - (OFFSET1 + OFFSET2 + OFFSET2) * 1 / 4;
 
             // 圆环处理，如果面积为负数，数值越大说明越偏左边；
-            if((OFFSET2 < -300)||(OFFSET2 > 300))
+            if ((OFFSET2 < -300) || (OFFSET2 > 300))
                 ServoDuty = Servo_Center_Mid - OFFSET2 / 7;
 
-            ServoCtrl(ServoDuty);     // 舵机PWM输出，转向
-
+            ServoCtrl(ServoDuty); // 舵机PWM输出，转向
 
             // SPEED正负标识方向，负数为正向
-            MotorDuty1 = MtTargetDuty + ECPULSE1 * 4 - (OFFSET1 + OFFSET2 + OFFSET2) / 10;        // 电机PWM
-            MotorDuty2 = MtTargetDuty - ECPULSE2 * 4 + (OFFSET1 + OFFSET2 + OFFSET2) / 10;        // 双电机差分，需要去掉abs
+            MotorDuty1 = MtTargetDuty + ECPULSE1 * 4 - (OFFSET1 + OFFSET2 + OFFSET2) / 10; // 电机PWM
+            MotorDuty2 = MtTargetDuty - ECPULSE2 * 4 + (OFFSET1 + OFFSET2 + OFFSET2) / 10; // 双电机差分，需要去掉abs
 
-//            MotorCtrl(MotorDuty1, MotorDuty2);        // 四轮电机驱动
-//             MotorCtrl(1800, 1800); // 电机PWM固定功率输出
-            MotorCtrl4w(1200, 1200, 1200, 1200);
-//             TFT_Show_Camera_Info();
+            //            MotorCtrl(MotorDuty1, MotorDuty2);        // 四轮电机驱动
+            //             MotorCtrl(1800, 1800); // 电机PWM固定功率输出
+            if (RAllPulse > 200)
+            {
+                MotorCtrl4w(0, 600, 0, 0);
+            }
+            else
+                (MotorCtrl4w(0, 1500, 0, 0));
+            //             TFT_Show_Camera_Info();
         }
         if (Game_Over)
         {
